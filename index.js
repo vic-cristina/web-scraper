1fa2009f032c5dad5671f3120a2391495f793230
Victoria Cristina <victoriacristina@protonmail.com>
2022-11-20 00:56:31 -0300

    add error handling fn

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
-const PORT = 8000;
-
+const PORT = 5003;
 const axios = require("axios");
 const cheerio = require("cheerio");
 const express = require("express");
 
 const app = express();
 
-const url = "https://www.ultrafarma.com.br/busca?q=climene";
+const estrSamples = [
+  "climene",
+  "oestrogel",
+  "ciclovular",
+  "espironolactona",
+  "bicalutamida",
+  "acetato de ciproterona",
+];
+const samples = [];
+const sampleAddr = [];
 
-axios(url).then((response) => {
-  const html = response.data;
-  const $ = cheerio.load(html);
+for (const estradiol of estrSamples) {
+  let url = `https://www.ultrafarma.com.br/busca?q=${estradiol}`;
+  console.log(url);
+  axios(url)
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
 
-  $(".product-price-sell", html).each(function () {
-    const price = $(this).text();
-    console.log(price);
-  });
-  $(".product-item-link", html).each(function () {
-    const url = $(this).attr("href");
-    console.log(url);
-  });
-});
+      $(".product-price-sell", html).each(function () {
+        const price = $(this).text();
+        console.log(estradiol, price);
+        samples.push(price);
+      });
+      $(".product-item-link", html).each(function () {
+        const url = $(this).attr("href");
+        console.log(url);
+        sampleAddr.push(url);
+      });
+    })
+    .catch((err) => console.log(err));
+}
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
